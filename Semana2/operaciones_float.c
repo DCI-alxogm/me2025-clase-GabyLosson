dc630f5dac5f866055be849158ffa708b1620207
GabyLosson <g.lossoncasanova@ugto.mx>
2025-08-13 16:01:25 -0600

    Add files via upload

---
+++ Semana2/operaciones_float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  float a=3, b=4, c=5, d=7;
+  float e;
+
+  printf("e=(a+b)*c/d=%f\n",(a+b)*c/d);
+  printf("e=((a+b)*c)/d=%f\n",((a+b)*c)/d);
+  printf("e=a+b*c/d=%f\n",a+b*c/d);
+  printf("e=a+(b*c)/d=%f\n",a+(b*c)/d);
+}

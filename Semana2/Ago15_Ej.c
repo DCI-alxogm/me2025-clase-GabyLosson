5333b34e671b17b0997c4db61361f45b82af23d0
GabyLosson <g.lossoncasanova@ugto.mx>
2025-08-15 18:52:41 -0600

    Add files via upload

---
+++ Semana2/Ago15_Ej.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+  float a, b, c, x1, x2, discr; 
+  printf("Ingrese el valor de A:"); 
+  scanf("%f", &a); 
+  scanf("%f", &b); 
+  scanf("%f", &c); 
+}

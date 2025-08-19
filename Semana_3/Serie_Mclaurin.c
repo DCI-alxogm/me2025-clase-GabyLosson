47c7f4fe09c3a0ec118f728b97aa6f5f6aa3a8c4
GabyLosson <g.lossoncasanova@ugto.mx>
2025-08-19 12:00:22 -0600

    Add files via upload

---
+++ Semana_3/Serie_Mclaurin.c
@@ -0,0 +1,14 @@
+#include <stio.h>
+#include <math.h>
+
+int main(){
+  float x, es, ea;
+  printf("Ingrese x: \n");
+  scanf("%f",&x);
+  printf("Proponga el error porcentual limite: \n");
+  scanf("%f",&es);
+  for(int n, ++n, ea<es){
+  }
+  
+  }
+}
